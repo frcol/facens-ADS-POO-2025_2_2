4cb13348dd7ca7d787c37f4823f19558879f323d
Fabio Colombini <fabiorcolombini@gmail.com>
2025-08-27 19:14:14 -0300

    primeiro comite

---
+++ src/Aula_1/Main_Demo1.java
@@ -0,0 +1,57 @@
+package Aula_1;
+
+import java.util.Scanner;
+
+public class Main_Demo1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int idade;
+        
+//        System.out.println("Digite a sua idade:");
+//        idade = entrada.nextInt();
+//        
+//        if (idade >= 18) {
+//            System.out.println("Entrada liberada");
+//        } else {
+//            System.out.println("Entrada NEGADA!!!!");
+//        }
+//        
+//        System.out.println("minha idade e "+idade+" anos");
+    
+//        System.out.println("[1]- Cadastrar\n [2]- Buscar\n[3]- Apagar\n [4]- Sair");
+//        int opt = entrada.nextInt();
+//        
+//        switch (opt) {
+//            case 1:
+//                System.out.println("Cadastrando...");
+//                break;
+//            case 2:
+//                System.out.println("Buscando...");
+//                break;
+//            case 3:
+//                System.out.println("Apagando...");
+//                break;
+//            case 4:
+//                System.out.println("Saind...");
+//                break;
+//            default:
+//                System.out.println("Opcao invalida");
+//        }
+    
+//        for (int i = 1; i <= 100; i+=2) {
+//            System.out.println(i);
+//        }
+        
+//          int num;
+//          do {
+//              System.out.println("Numero:");
+//              num = entrada.nextInt();
+//          }while(num >= 0);
+          
+          
+          double valor = 3.33;
+          System.out.printf("%.3f", valor);
+    }
+    
+    
+}
