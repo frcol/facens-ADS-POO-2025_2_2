b9fb2ac04d35124f37c7a0930746e776e65adf7a
Fabio Colombini <fabiorcolombini@gmail.com>
2025-09-17 19:47:52 -0300

    aula 6

---
+++ src/Aula_6/Main_Pessoa.java
@@ -0,0 +1,17 @@
+package Aula_6;
+
+public class Main_Pessoa {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa();
+        
+        Pessoa p1 = new Pessoa("Fabio", 52);
+        p1.emprestar("Desventuras em serie");
+        
+        Pessoa p2 = new Pessoa("Joao", 33, "Star Wars", 30);
+        p2.emprestar("Ensaio sobre a cegueira", 125);
+        
+        p.imprimir();
+        p1.imprimir();
+        p2.imprimir();
+    }
+}
---
+++ src/Aula_6/Pessoa.java
@@ -0,0 +1,43 @@
+package Aula_6;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String livro;
+    private int diasDevolucao;
+    
+    public Pessoa() {
+        
+    }
+    
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    
+    public Pessoa(String nome, int idade, String livro, int dias) {
+        this.nome = nome;
+        this.idade = idade;
+        this.livro = livro;
+        this.diasDevolucao = dias;
+    }
+    
+    public void emprestar(String livro) {
+        this.livro = livro;
+        this.diasDevolucao = 20;
+    }
+    
+    public void emprestar(String livro, int dias) {
+        this.livro = livro;
+        this.diasDevolucao = dias   ;
+    }
+    
+    public void imprimir() {
+        String msg = "\n====================="
+                    +"\nNome: "+nome
+                    +"\nIdade: "+idade
+                    +"\nLivro: "+livro
+                    +"\nDias: "+diasDevolucao;
+        System.out.println(msg);
+    }
+} 
